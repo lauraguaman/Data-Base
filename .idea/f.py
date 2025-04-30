29cbe86ba9a86c6abed9f3670b62b40f3b321e13
lauraguaman <0guaml23@solent.ac.uk>
2025-04-30 12:44:20 +0100

    python-sql first example

---
+++ .idea/f.py
@@ -0,0 +1,16 @@
+
+
+# import sqlite3
+# db = sqlite3.connect('/Applications/SQLiteStudio/week 1 example/mysportdatabase.db')
+# cursor = db.cursor()
+# sql_query = "SELECT * \
+# 		   FROM dept"
+# cursor.execute(sql_query)
+# emp_row = cursor.fetchone()
+# print(emp_row)
+# db.close()
+
+print("hello world")
+
+
+
